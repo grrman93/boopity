2e54e2b39cf93dfa1f3c19d782912eb23a00b9e4
Greg Roche <grroche.us@gmail.com>
2016-08-09 13:50:03 -0700

    WORKING 4 WAY MESH CONNECTION

--- server.js
+++ server.js
@@ -20,37 +20,88 @@ io.on('connection', function(socket) {
   counter--;
 
   socket.emit('ID', socket.id);
+  console.log('s' + socket.id + ' created')
+  socket.broadcast.emit('s' + socket.id + ' created')
 
   console.log('A user connected with socket id', socket.id);
   // socket.broadcast.emit('ask');
   // console.log('asking...')
 
   socket.on('disconnect', function() {
+    //******* TEMPORARY *******//
+    counter++;
+    console.log(counter)
+    //************************ //
     console.log('A user disconnected with socket id', socket.id);
   });
+
+  // peer3 to peer4 connection handling
+  socket.on('peer3 offer to peer4', function(data) { socket.broadcast.emit('peer3 offer to peer4', data); })
+  socket.on('peer4 answer to peer3', function(data) { socket.broadcast.emit('peer4 answer to peer3', data) })
+  // peer2 to peer4 connection handling
+  socket.on('peer2 offer to peer4', function(data) { socket.broadcast.emit('peer2 offer to peer4', data); })
+  socket.on('peer4 answer to peer2', function(data) {socket.broadcast.emit('peer4 answer to peer2', data);})
+  // peer1 to peer4 connection handling
+  socket.on('peer1 offer to peer4', function(data) { socket.broadcast.emit('peer1 offer to peer4', data); })
+  socket.on('peer4 answer to peer1', function(data) {socket.broadcast.emit('peer4 answer to peer1', data);})
+
+
+  // peer2 to peer3 connection handling
+  socket.on('peer2 offer to peer3', function(data) { socket.broadcast.emit('peer2 offer to peer3', data); })
+  socket.on('peer3 answer to peer2', function(data) {socket.broadcast.emit('peer3 answer to peer2', data);})
+  // peer1 to peer3 connection handling
+  socket.on('peer1 offer to peer3', function(data) { socket.broadcast.emit('peer1 offer to peer3', data); })
+  socket.on('peer3 answer to peer1', function(data) {socket.broadcast.emit('peer3 answer to peer1', data);})
+
+  //peer 1 to peer2 connection handling
+  socket.on('peer1 offer to peer2', function(data) { socket.broadcast.emit('peer1 offer to peer2', data); })
+  socket.on('peer2 answer to peer1', function(data) {socket.broadcast.emit('peer2 answer to peer1', data);})
+
+  // connection handling
+  socket.on('4 connected to 2', function() {
+    console.log('4 connected to 2')
+    socket.broadcast.emit('4 connected to 2')
+  }) 
+
+  socket.on('4 connected to 1', function() {
+    socket.broadcast.emit('4 connected to 1')
+  })
+
+  socket.on('3 connected to 1', function() {
+    socket.broadcast.emit('3 connected to 1')
+  })
+
+
 
   socket.on('offer', function(data) {
     console.log('making an offer')
     socket.broadcast.emit('accept offer', data);
   });
-
   socket.on('answer', function(data) {
     console.log('making an answer')
     socket.broadcast.emit('accept answer', data)
   });
+
+  socket.on('connected s3', () => { socket.emit('connected s3') })
+  socket.on('connected s2', () => { socket.emit('connected s2') })
+
+  // Offers
+  socket.on('s1 answer', (data) => { socket.broadcast.emit('accept s1 answer', data) })
+  socket.on('s2 answer', (data) => { socket.broadcast.emit('accept s2 answer', data) })
+  socket.on('s3 answer', (data) => { socket.broadcast.emit('accept s3 answer', data) })
+
+  // Answers
+  socket.on('s3 offer', (data) => { socket.broadcast.emit('accept s3 offer', data) })
+  socket.on('s2 offer', (data) => { socket.broadcast.emit('accept s2 offer', data) })
+  socket.on('s4 offer', (data) => { socket.broadcast.emit('accept s4 offer', data) })
+
+
+  // sRoutes.s3_connected(())
+  
+
 })
 
-sRoutes = {}
 
-// Offers
-sRoutes.s1_Offer = () => { socket.on('socket 1 offer') }
-sRoutes.s2_Offer = () => { socket.on('scoket 2 offer') }
-sRoutes.s3_Offer = () => { socket.on('socket 3 offer') }
-
-// Answers
-sRoutes.s2Offer = () => { socket.on('socket 2 answer') }
-sRoutes.s3Offer = () => { socket.on('scoket 3 answer') }
-sRoutes.s4Offer = () => { socket.on('socket 4 answer') }
 
 
 
--- src/app.js
+++ src/app.js
@@ -2,67 +2,145 @@ var Peer = require('simple-peer')
 // var p = new Peer({ initiator: location.hash === '#1', trickle: false })
 var io = require('socket.io-client');
 
+
 var socket = io();
 
 socket.on('ID', function(socketId) {
   if (socketId === 1) {
     console.log('I AM PEER ', socketId);
-    var p2 = new Peer({ initiator: true, trickle: false })
-    var p3 = new Peer({ initiator: true, trickle: false })
+    // first offer
     var p4 = new Peer({ initiator: true, trickle: false })
+    peerHandler(p4, true, 4, socketId);
 
-    peerHandler(p4, true, 4);
-    peerHandler(p3, true, 3);
-    peerHandler(p2, true, 2);
+    // second offer, need to wait for first to finish
+    socket.on('4 connected to 1', function() {
+      var p3 = new Peer({ initiator: true, trickle: false })
+      peerHandler(p3, true, 3, socketId);
+    })
+
+    // third offer, need to wait for first to finish
+    socket.on('3 connected to 1', function() {
+      var p2 = new Peer({ initiator: true, trickle: false })
+      peerHandler(p2, true, 2, socketId);
+    })
   } else if (socketId === 2) {
     console.log('I AM PEER ', socketId);
-    var p1 = new Peer({ initiator: false, trickle: false })
-    var p3 = new Peer({ initiator: true, trickle: false })
-    var p4 = new Peer({ initiator: true, trickle: false })
 
-    peerHandler(p4, true, 4);
-    peerHandler(p3, true, 3);
-    peerHandler(p1, false, 1);
+    // first offer
+    var p4 = new Peer({ initiator: true, trickle: false })
+    peerHandler(p4, true, 4, socketId); 
+
+    // second offer, need to wait to for first to finish
+    socket.on('4 connected to 2', function(){
+      console.log('peer2 now initializing connection to peer3')
+      var p3 = new Peer({ initiator: true, trickle: false })
+      peerHandler(p3, true, 3, socketId);
+    })
+
+    // answer, need to wait for 3 to finish
+    socket.on('3 connected to 1', function() {
+      var p1 = new Peer({ initiator: false, trickle: false })
+      peerHandler(p1, false, 1, socketId);
+    })
+
+    
+
+    // // var p1 = new Peer({ initiator: false, trickle: false })
+    // peerHandler(p1, false, 1);
   } else if (socketId === 3) {
     console.log('I AM PEER ', socketId);
-    var p1 = new Peer({ initiator: false, trickle: false })
-    var p2 = new Peer({ initiator: false, trickle: false })
-    var p4 = new Peer({ initiator: true, trickle: false })
 
-    peerHandler(p4, true, 4);
-    peerHandler(p2, false, 2);
-    peerHandler(p1, false, 1);
+
+    // give offer
+    var p4 = new Peer({ initiator: true, trickle: false })
+    peerHandler(p4, true, 4, socketId);
+
+    // first answer, need to wait for 4 to connect first
+    socket.on('4 connected to 2', function(){
+      console.log("s3 got news of s4/s2 connection, now initializing connection to peer2")
+      var p2 = new Peer({ initiator: false, trickle: false })
+      peerHandler(p2, false, 2, socketId);
+    })
+
+    // second answer need to wait for 4 to connect first
+    socket.on('4 connected to 1', function() {
+      var p1 = new Peer({ initiator: false, trickle: false })
+      peerHandler(p1, false, 1, socketId);
+    })
+    // var p1 = new Peer({ initiator: false, trickle: false })
+
+    // peerHandler(p2, false, 2);
+    // peerHandler(p1, false, 1);
   } else if (socketId === 4) {
     console.log('I AM PEER ', socketId);
-    var p1 = new Peer({ initiator: false, trickle: false })
-    var p2 = new Peer({ initiator: false, trickle: false })
-    var p3 = new Peer({ initiator: false, trickle: false })
 
-    peerHandler(p3, false, 3);
-    peerHandler(p2, false, 2);
-    peerHandler(p1, false, 1);
+    socket.on('s3 created', function() {
+      console.log('s4 saw s3 created');
+      // this will recieve offer
+      var p3 = new Peer({ initiator: false, trickle: false })
+      peerHandler(p3, false, 3, socketId);
+    })
+
+    socket.on('s2 created', function() {
+      console.log('s4 saw s2 created');
+      var p2 = new Peer({ initiator: false, trickle: false })
+      peerHandler(p2, false, 2, socketId);
+    })
+
+    socket.on('s1 created', function() {
+      console.log('s4 saw s1 created');
+      var p1 = new Peer({ initiator: false, trickle: false })
+      peerHandler(p1, false, 1, socketId);
+    })
+
+
+
+
+    // socket.on('connected s3', function() {
+    //   console.log('recvieved message')
+    // })
+    // listen();
+    // // socket.on('s3 connected', () => {
+    // var p3 = new Peer({ initiator: false, trickle: false })
+    // peerHandler(p3, false, 3);
+
+    // var p2 = new Peer({ initiator: false, trickle: false })
+    // peerHandler(p2, false, 2);
+    // // })
+    // socket.on('s2 created', () => {
+    //   console.log("s4 got news of s2's creation")
+    // })
+
+
+    // socket.on('connected s3', () => {
+    //   console.log('connected s3')
+    // })
+    // socket.on('s3 offer', (data) => { 
+    // })
+
+    // var p3 = new Peer({ initiator: false, trickle: false })
+    // var p2 = new Peer({ initiator: false, trickle: false })
+    // var p1 = new Peer({ initiator: false, trickle: false })
+
+    // peerHandler(p3, false, 3);
+    // peerHandler(p2, false, 2);
+    // peerHandler(p1, false, 1);
   }
 });
 
-
-// var io = require('socket.io');
-
-// var p = new Peer({ initiator: true, trickle: false })
-
-
-
-function peerHandler(p, initiatorCheck, ID) {
-  console.log("instantiated");
+function peerHandler(p, initiatorCheck, ID, sID) {
+  console.log("instantiated, ID: ", ID);
   p.on('error', function (err) { console.log('error', err) })
 
-  if(!initiatorCheck) {
-    peerInitiator(p, ID);
+  if (!initiatorCheck) {
+    peerReciever(p, ID, sID);
   } else {
-    peerReciever(p, ID);
+    peerInitiator(p, ID, sID);
   }
 
   p.on('connect', function () {
-    console.log('CONNECT')
+    console.log(sID + ' connected to ' + ID)
+    socket.emit(sID + ' connected to ' + ID)
     p.send('whatever' + Math.random())
   })
 
@@ -71,36 +149,74 @@ function peerHandler(p, initiatorCheck, ID) {
   })
 };
 
-function peerReciever(p, ID) {
-  socket.on('accept answer', function (data){
-    console.log('Socket ' + ID + ' recieved an offer');
+function peerInitiator(p, ID, sID) {
+  console.log('peer' + sID + ' made initiator for peer' + ID)
+  socket.on('peer' + ID + ' answer to peer' + sID, function(data) {
     p.signal(JSON.parse(data));
   })
+  // socket.on('accept s'+ID+' answer', function (data){
+  //   console.log('Socket ' + ID + ' recieved an offer');
+  //   p.signal(JSON.parse(data));
+  // })
+
+  // socket.on('accept s1 answer', function (data){
+  //   console.log('Socket ' + ID + ' recieved an answer s1');
+  //   p.signal(JSON.parse(data));
+  // })
+
+  // socket.on('accept s2 answer', function (data){
+  //   console.log('Socket ' + ID + ' recieved an answer s2');
+  //   p.signal(JSON.parse(data));
+  // })
+
+  // socket.on('accept s3 answer', function (data){
+  //   console.log('Socket ' + ID + ' recieved an answer from s3');
+  //   p.signal(JSON.parse(data));
+  // })
 
   p.on('signal', function(data) {
     console.log('TYPE TEST: ', data.type);
     // console.log('SIGNAL', JSON.stringify(data))
     
     if(data.type === 'offer') {
-      console.log('offer tripped, socketID: ', ID)
-      socket.emit('offer', JSON.stringify(data));
+      console.log('peer' + sID + ' sent offer to peer' + ID)
+      socket.emit('peer' + sID + ' offer to peer' + ID, JSON.stringify(data));
     }
   })
 };
 
-function peerInitiator(p, ID) {
-  socket.on('accept offer', function (data){
-    console.log('Socket ' + ID + ' recieved an answer');
+function peerReciever(p, ID, sID) {
+  console.log('peer' + sID + ' made reciever for peer' + ID)
+  socket.on('peer' + ID + ' offer to peer' + sID, function(data) {
     p.signal(JSON.parse(data));
   })
+  // socket.on('accept s'+ ID +' offer', function (data){
+  //   console.log('Socket ' + ID + ' recieved an answer');
+  //   p.signal(JSON.parse(data));
+  // })
+
+  // socket.on('accept s3 offer', function (data){
+  //   console.log('Socket ' + ID + ' recieved an offer from s3');
+  //   p.signal(JSON.parse(data));
+  // })
+
+  // socket.on('accept s2 offer', function (data){
+  //   console.log('Socket ' + ID + ' recieved an offer s2');
+  //   p.signal(JSON.parse(data));
+  // })
+
+  // socket.on('accept s4 offer', function (data){
+  //   console.log('Socket ' + ID + ' recieved an offer s4');
+  //   p.signal(JSON.parse(data));
+  // })
 
   p.on('signal', function(data) {
     console.log('TYPE TEST: ', data.type);
     // console.log('SIGNAL', JSON.stringify(data))
 
     if(data.type === 'answer') {
-      console.log('answer tripped, socketID: ', ID)
-      socket.emit('answer', JSON.stringify(data));
+      console.log('peer' + sID + ' sent answer to peer' + ID);
+      socket.emit('peer' + sID + ' answer to peer' + ID, JSON.stringify(data));
     }
   })
 };
